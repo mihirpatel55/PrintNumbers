de36a8a4f03ac28e503a0692ca61bd6f723d8b8a
mihirpatel55 <50714129+mihirpatel55@users.noreply.github.com>
2019-05-16 13:25:56 -0400

    Uploaded from desktop
    Two files Class1 and Program.cs will print numbers that takes integer input from command line as argument and print 1 through n numbers as Buzz (if it's divided by 5 ) or fuzz ( if it's divided by 3)

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintFuzzBuzzNumbers
+{
+    public class Class1
+    {
+        public void RunMethod(int q)
+        {
+            for (int i = 1; i <= q; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(" FUzz ");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(" Buzz ");
+                }
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrintFuzzBuzzNumbers;
+namespace PrintNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(args[0]);
+            Class1 prntnum = new Class1();
+            prntnum.RunMethod(num);
+        }
+    }
+}
